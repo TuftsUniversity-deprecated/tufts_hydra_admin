3eba6e784a70c7110ef86689bb807c434b55ebf9
Mike Korcynski <mkorcy@gmail.com>
2013-04-09 13:12:50 -0400

    initial pass at config code, not done

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -8,7 +8,7 @@ class CatalogController < ApplicationController
   # These before_filters apply the hydra access controls
   before_filter :enforce_show_permissions, :only=>:show
   # This applies appropriate access controls to all solr queries
-  CatalogController.solr_search_params_logic += [:add_access_controls_to_solr_params]
+  #CatalogController.solr_search_params_logic += [:add_access_controls_to_solr_params]
   # This filters out objects that you want to exclude from search results, like FileAssets
   CatalogController.solr_search_params_logic += [:exclude_unwanted_models]
 
@@ -47,13 +47,16 @@ class CatalogController < ApplicationController
     #
     # :show may be set to false if you don't want the facet to be drawn in the 
     # facet bar
-    config.add_facet_field solr_name('object_type', :facetable), :label => 'Format' 
-    config.add_facet_field solr_name('pub_date', :facetable), :label => 'Publication Year' 
-    config.add_facet_field solr_name('subject_topic', :facetable), :label => 'Topic', :limit => 20 
-    config.add_facet_field solr_name('language', :facetable), :label => 'Language', :limit => true 
-    config.add_facet_field solr_name('lc1_letter', :facetable), :label => 'Call Number' 
-    config.add_facet_field solr_name('subject_geo', :facetable), :label => 'Region' 
-    config.add_facet_field solr_name('subject_era', :facetable), :label => 'Era'  
+    #  "names_facet" => 7,
+    #          "year_facet" => 7,
+    #                  "subject_facet" =>7,
+    #                          "collection_facet" =>7,
+    #                                  "object_type_facet" => 7
+    config.add_facet_field solr_name('names_facet', :facetable), :label => 'Names', :limit => 7 
+    config.add_facet_field solr_name('year_facet', :facetable), :label => 'Year', :limit => 7 
+    config.add_facet_field solr_name('subject_facet', :facetable), :label => 'Subject', :limit => 7 
+    config.add_facet_field solr_name('collection_facet', :facetable), :label => 'Collection', :limit => 7
+    config.add_facet_field solr_name('object_type_facet', :facetable), :label => 'Format', :limit => 7
 
     # Have BL send all facet field names to Solr, which has been the default
     # previously. Simply remove these lines if you'd rather use Solr request
@@ -71,9 +74,10 @@ class CatalogController < ApplicationController
     config.add_index_field solr_name('author_vern', :stored_searchable, type: :string), :label => 'Author:' 
     config.add_index_field solr_name('format', :symbol), :label => 'Format:' 
     config.add_index_field solr_name('language', :stored_searchable, type: :string), :label => 'Language:'
-    config.add_index_field solr_name('published', :stored_searchable, type: :string), :label => 'Published:'
+    #config.add_index_field solr_name('published', :stored_searchable, type: :string), :label => 'Published:'
     config.add_index_field solr_name('published_vern', :stored_searchable, type: :string), :label => 'Published:'
     config.add_index_field solr_name('lc_callnum', :stored_searchable, type: :string), :label => 'Call number:'
+    config.add_index_field solr_name('temporal_teim', :stored_searchable, type: :string), :label => 'Pub Date:'
 
     # solr fields to be displayed in the show (single result) view
     #   The ordering of the field names is the order of the display 
